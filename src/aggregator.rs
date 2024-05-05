d37370178ecbf8edc3ff5bd5cf14295ab9bb4fb7
Mateusz Walewski <hraboa77@gmail.com>
2024-05-05 18:27:24 +0200

    The calculation engine generalization and gathering partial results from nodes

--- src/aggregator.rs
+++ src/aggregator.rs
@@ -4,72 +4,95 @@ use std::{
     thread,
 };
 
-pub fn sum<T: NumericType<T>>(data: &ColumnData<T>) -> T {
-    let rx = engine(data, sum_func);
-    rx.iter().fold(T::default(), |acc, e| acc + &e)
+pub fn sum<T: NumericType<T>>(data: &ColumnData<T>) -> Option<T> {
+    let rx = engine(data, sum_on_node);
+    gather_from_nodes(rx, Some(T::default()), |acc, x| acc?.checked_add(x?))
 }
 
-pub fn min<T: NumericType<T>>(data: &ColumnData<T>) -> T {
-    let rx = engine(data, min_func);
-    rx.iter()
-        .fold(None, |min, x| match min {
-            None => Some(x),
-            Some(curr_min) => Some(tools::partial_min(curr_min, x).unwrap()),
-        })
-        .expect("Should be any value")
+pub fn min<T: NumericType<T>>(data: &ColumnData<T>) -> Option<T> {
+    let rx = engine(data, min_on_node);
+    gather_from_nodes(rx, None, |min, x| match min {
+        None => x,
+        Some(curr_min) => tools::partial_min(curr_min, x?),
+    })
 }
 
-pub fn max<T: NumericType<T>>(data: &ColumnData<T>) -> T {
-    let rx = engine(data, max_func);
-    rx.iter()
-        .fold(None, |max, x| match max {
-            None => Some(x),
-            Some(curr_max) => Some(tools::partial_max(curr_max, x).unwrap()),
-        })
-        .expect("Should be any value")
+pub fn max<T: NumericType<T>>(data: &ColumnData<T>) -> Option<T> {
+    let rx = engine(data, max_on_node);
+    gather_from_nodes(rx, None, |max, x| match max {
+        None => x,
+        Some(curr_max) => tools::partial_max(curr_max, x?),
+    })
 }
 
-fn engine<T: NumericType<T>, F>(data: &ColumnData<T>, func: F) -> mpsc::Receiver<T>
+fn gather_from_nodes<T: NumericType<T>, F>(
+    receiver: mpsc::Receiver<Option<T>>,
+    init: Option<T>,
+    func: F,
+) -> Option<T>
 where
-    F: Fn(Arc<Vec<T>>, usize, usize) -> T + Send + 'static + Copy,
+    F: FnOnce(Option<T>, Option<T>) -> Option<T>,
+    F: Send + Copy + 'static,
+{
+    receiver.iter().fold(init, |max, x| func(max, x))
+}
+
+fn engine<T: NumericType<T>, F>(data: &ColumnData<T>, func: F) -> mpsc::Receiver<Option<T>>
+where
+    F: Fn(Arc<Vec<T>>, usize, usize) -> Option<T> + Send + 'static + Copy,
 {
     let (tx, rx) = mpsc::channel();
     let ranges = tools::calculate_ranges(data.data().len(), constants::NUMBER_OF_NODES);
     let data_threaded_ref = data.data();
+    let mut threads = Vec::<thread::JoinHandle<()>>::with_capacity(constants::NUMBER_OF_NODES);
     for range in ranges {
         let data_safe = data_threaded_ref.clone();
         let tx1 = tx.clone();
-        thread::spawn(move || {
+        let th = thread::spawn(move || {
             let result = func(data_safe, range.0, range.1);
             tx1.send(result).unwrap();
         });
+        threads.push(th);
+    }
+    for thread in threads {
+        thread.join().unwrap()
     }
     drop(tx);
     rx
 }
 
-pub fn sum_func<T: NumericType<T>>(data_safe: Arc<Vec<T>>, begin: usize, end: usize) -> T {
+pub fn sum_on_node<T: NumericType<T>>(
+    data_safe: Arc<Vec<T>>,
+    begin: usize,
+    end: usize,
+) -> Option<T> {
     data_safe[begin..end]
         .iter()
-        .fold(T::default(), |acc, e| acc + e)
+        .fold(Some(T::default()), |acc, &e| acc?.checked_add(e))
 }
 
-pub fn min_func<T: NumericType<T>>(data_safe: Arc<Vec<T>>, begin: usize, end: usize) -> T {
-    *data_safe[begin..end]
+pub fn min_on_node<T: NumericType<T>>(
+    data_safe: Arc<Vec<T>>,
+    begin: usize,
+    end: usize,
+) -> Option<T> {
+    data_safe[begin..end]
         .iter()
-        .fold(None, |min, x| match min {
+        .fold(None, |min, &x| match min {
             None => Some(x),
-            Some(curr_min) => Some(tools::partial_min(curr_min, x).unwrap()),
+            Some(curr_min) => tools::partial_min(curr_min, x),
         })
-        .expect("Should be any value")
 }
 
-pub fn max_func<T: NumericType<T>>(data_safe: Arc<Vec<T>>, begin: usize, end: usize) -> T {
-    *data_safe[begin..end]
+pub fn max_on_node<T: NumericType<T>>(
+    data_safe: Arc<Vec<T>>,
+    begin: usize,
+    end: usize,
+) -> Option<T> {
+    data_safe[begin..end]
         .iter()
-        .fold(None, |max, x| match max {
+        .fold(None, |max, &x| match max {
             None => Some(x),
-            Some(curr_max) => Some(tools::partial_max(curr_max, x).unwrap()),
+            Some(curr_max) => tools::partial_max(curr_max, x),
         })
-        .expect("Should be any value")
 }
