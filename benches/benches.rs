ddf4169bfe1db0108a46590fe4f36616bd31d932
MateuszWalewski <32540764+MateuszWalewski@users.noreply.github.com>
2024-05-08 16:24:07 +0200

    Addition of unit tests and benchmarks for variance, stddev, moment_i, moment_ii (#21)

--- benches/benches.rs
+++ benches/benches.rs
@@ -65,7 +65,6 @@ fn bench_column_sum_x2_4_nodes(c: &mut Criterion) {
     group.finish();
 }
 
-
 fn bench_column_min_1_node(c: &mut Criterion) {
     let container: Vec<f64> = common::load_data("data_3M.csv");
     let column = Column::new();
@@ -98,7 +97,6 @@ fn bench_column_min_4_nodes(c: &mut Criterion) {
 
     group.finish();
 }
-
 
 fn bench_column_max_1_node(c: &mut Criterion) {
     let container: Vec<f64> = common::load_data("data_3M.csv");
@@ -133,6 +131,137 @@ fn bench_column_max_4_nodes(c: &mut Criterion) {
     group.finish();
 }
 
+fn bench_column_variance_1_node(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("variance_t", |b| b.iter(|| column.variance_t(1)));
+
+    group.finish();
+}
+
+fn bench_column_variance_2_nodes(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("variance_t", |b| b.iter(|| column.variance_t(2)));
+
+    group.finish();
+}
+
+fn bench_column_variance_4_nodes(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("variance_t", |b| b.iter(|| column.variance_t(4)));
+
+    group.finish();
+}
+
+fn bench_column_stddev_1_node(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("stddev_t", |b| b.iter(|| column.stddev_t(1)));
+
+    group.finish();
+}
+
+fn bench_column_stddev_2_nodes(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("stddev_t", |b| b.iter(|| column.stddev_t(2)));
+
+    group.finish();
+}
+
+fn bench_column_stddev_4_nodes(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("stddev_t", |b| b.iter(|| column.stddev_t(4)));
+
+    group.finish();
+}
+
+fn bench_column_moment_i_1_node(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("moment_i_t", |b| b.iter(|| column.moment_i_t(1)));
+
+    group.finish();
+}
+
+fn bench_column_moment_i_2_nodes(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("moment_i_t", |b| b.iter(|| column.moment_i_t(2)));
+
+    group.finish();
+}
+
+fn bench_column_moment_i_4_nodes(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("moment_i_t", |b| b.iter(|| column.moment_i_t(4)));
+
+    group.finish();
+}
+
+fn bench_column_moment_ii_1_node(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("moment_ii_t", |b| b.iter(|| column.moment_ii_t(1)));
+
+    group.finish();
+}
+
+fn bench_column_moment_ii_2_nodes(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("moment_ii_t", |b| b.iter(|| column.moment_ii_t(2)));
+
+    group.finish();
+}
+
+fn bench_column_moment_ii_4_nodes(c: &mut Criterion) {
+    let container: Vec<f64> = common::load_data("data_3M.csv");
+    let column = Column::new();
+    let column = column.add_data(container);
+    let mut group = c.benchmark_group("column_sum");
+
+    group.bench_function("moment_ii_t", |b| b.iter(|| column.moment_ii_t(4)));
+
+    group.finish();
+}
 
 criterion_group!(
     benches,
@@ -142,6 +271,18 @@ criterion_group!(
     bench_column_sum_x2_1_node,
     bench_column_sum_x2_2_nodes,
     bench_column_sum_x2_4_nodes,
+    bench_column_variance_1_node,
+    bench_column_variance_2_nodes,
+    bench_column_variance_4_nodes,
+    bench_column_stddev_1_node,
+    bench_column_stddev_2_nodes,
+    bench_column_stddev_4_nodes,
+    bench_column_moment_i_1_node,
+    bench_column_moment_i_2_nodes,
+    bench_column_moment_i_4_nodes,
+    bench_column_moment_ii_1_node,
+    bench_column_moment_ii_2_nodes,
+    bench_column_moment_ii_4_nodes,
     bench_column_min_1_node,
     bench_column_min_2_nodes,
     bench_column_min_4_nodes,
